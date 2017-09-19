1902134366f173d3b3507bce47320c6d95259f20
Razum Force <razum.force@gmail.com>
2017-09-20 00:41:48 +0300

    add putting automatically by random 1 x 4 cell enemy ship on field and generating surround cells array

--- ship.rb
+++ ship.rb
@@ -50,6 +50,125 @@ class Ship
 		@overall << ship.sort!			
 	end
 
+	def add_ships
+		coords_array = init_positions
+		#ДОБАВЛЯЕМ 1 по 4 КЛЕТКИ
+		ship = []
+		surround = []
+		col, row = coords_array.shuffle.pop
+		ship << [col, row]
+		dice_x, dice_y = coords_array.shuffle.pop
+		if dice_x > dice_y #ТОГДА ВЕРТИКАЛЬНО
+			if row <=2
+				surround << [col-1, row] if col-1 > -1
+				surround << [col+1, row] if col+1 < Draw::Game::BOARD_SIZE[0]
+				surround << [col-1, row-1] if col-1 > -1 && row-1 > -1
+				surround << [col+1, row-1] if col+1 < Draw::Game::BOARD_SIZE[0] && row-1 > -1
+				surround << [col, row-1] if row-1 > -1
+				surround << [col-1, row+4] if col-1 > -1 && row+4 < Draw::Game::BOARD_SIZE[1]
+				surround << [col+1, row+4] if col+1 < Draw::Game::BOARD_SIZE[0] && row+4 < Draw::Game::BOARD_SIZE[1]
+				surround << [col, row+4] if row+4 < Draw::Game::BOARD_SIZE[1]
+				3.times do |i|
+					ship << [col, row+i+1]
+					surround << [col-1, row+i+1] if col-1 > -1
+					surround << [col+1, row+i+1] if col+1 < Draw::Game::BOARD_SIZE[0]
+				end
+			else
+				surround << [col-1, row] if col-1 > -1
+				surround << [col+1, row] if col+1 < Draw::Game::BOARD_SIZE[0]
+				surround << [col-1, row+1] if col-1 > -1 && row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col+1, row+1] if col+1 < Draw::Game::BOARD_SIZE[0] && row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col, row+1] if row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col-1, row-4] if col-1 > -1 && row-4 > -1
+				surround << [col+1, row-4] if col+1 < Draw::Game::BOARD_SIZE[0] && row-4 > -1
+				surround << [col, row-4] if row-4 > -1
+				3.times do |i|
+					ship << [col, row-i-1]
+					surround << [col-1, row-i-1] if col-1 > -1
+					surround << [col+1, row-i-1] if col+1 < Draw::Game::BOARD_SIZE[0]
+				end
+			end
+		else #ТОГДА ГОРИЗОНТАЛЬНО
+			if col <=2
+				surround << [col, row-1] if row-1 > -1
+				surround << [col, row+1] if row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col-1, row-1] if col-1 > -1 && row-1 > -1
+				surround << [col-1, row+1] if col-1 > -1 && row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col-1, row] if col-1 > -1
+				surround << [col+4, row-1] if col+4 < Draw::Game::BOARD_SIZE[0] && row-1 > -1
+				surround << [col+4, row+1] if col+4 < Draw::Game::BOARD_SIZE[0] && row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col+4, row] if col+4 < Draw::Game::BOARD_SIZE[0]
+				3.times do |i|
+					ship << [col+i+1, row]
+					surround << [col+i+1, row-1] if row-1 > -1
+					surround << [col+i+1, row+1] if row+1 < Draw::Game::BOARD_SIZE[1]
+				end
+			else
+				surround << [col, row-1] if row-1 > -1
+				surround << [col, row+1] if row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col+1, row-1] if col+1 < Draw::Game::BOARD_SIZE[0] && row-1 > -1
+				surround << [col+1, row+1] if col+1 < Draw::Game::BOARD_SIZE[0] && row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col+1, row] if col+1 < Draw::Game::BOARD_SIZE[0]
+				surround << [col-4, row-1] if col-4 > -1 && row-1 > -1
+				surround << [col-4, row+1] if col-4 > -1 && row+1 < Draw::Game::BOARD_SIZE[1]
+				surround << [col-4, row] if col-4 > -1
+				3.times do |i|
+					ship << [col-i-1, row]
+					surround << [col-i-1, row-1] if row-1 > -1
+					surround << [col-i-1, row+1] if row+1 < Draw::Game::BOARD_SIZE[1]
+				end
+			end
+		end
+		@overall << ship.sort!
+		coords_array -= ship
+		coords_array -= surround
+		
+		p "coords_array:"
+		p coords_array
+		p "ship enemy"
+		p ship
+		p "surround"
+		p surround
+
+		# #ДОБАВЛЯЕМ 2 по 3 КЛЕТКИ
+		# ship = []
+		# col, row = coords_array.shuffle.pop
+		# ship << [col, row]
+		# dice_x, dice_y = coords_array.shuffle.pop
+		# if dice_x > dice_y #ТОГДА ВЕРТИКАЛЬНО
+		# 	if row <=2
+		# 		3.times do |i|
+		# 			ship << [col, row+i]
+		# 		end
+		# 	else
+		# 		3.times do |i|
+		# 			ship << [col, row-i]
+		# 		end
+		# 	end
+		# else #ТОГДА ГОРИЗОНТАЛЬНО
+		# 	if col <=2
+		# 		3.times do |i|
+		# 			ship << [col+i, row]
+		# 		end
+		# 	else
+		# 		3.times do |i|
+		# 			ship << [col-i, row]
+		# 		end
+		# 	end
+		# end
+		# @overall << ship.sort!
+
+	end
+
+	def mark_on_field(enemy_battle_field)
+		@overall.each do |ship|
+			ship.each do |coord|
+				col, row = coord
+				enemy_battle_field[col][row] ||= 1
+			end
+		end
+	end
+
 	private
 	def define_sides(l = 1, r = 1, t = 1, b = 1, sides, col, row, battle_field)
 		sides["bottom"] = battle_field[col][row-b] if (row-b) > -1
@@ -68,5 +187,21 @@ class Ship
 		right_bottom = battle_field[col+1][row+1] if (col+1 < Draw::Game::BOARD_SIZE[0]) && (row+1 < Draw::Game::BOARD_SIZE[1])
 		[left_top, right_top, left_bottom, right_bottom]
 	end
+
+	def init_positions
+			init = Array.new(Draw::Game::BOARD_SIZE[0]*Draw::Game::BOARD_SIZE[1])
+			x = 0
+			i = 0
+			Draw::Game::BOARD_SIZE[0].times do
+				y = 0
+				Draw::Game::BOARD_SIZE[1].times do
+					init[i] = [x,y]
+					i += 1
+					y += 1
+				end
+				x += 1
+			end
+			return init
+		end
 
 end
